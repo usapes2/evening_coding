65d3e0ac4b704a7ee740078a4815b1d8fb8db75c
=Uladzimir Sapeshka <usapeshka@gmail.com>
2018-03-06 00:37:09 -0600

    Linked List search debugging

--- linkedListSearch.c
+++ linkedListSearch.c
@@ -4,6 +4,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+void skipRestOfInput(FILE *stream)
+{
+	char restOfLine[256];
+	int rolLength = sizeof(restOfLine) / sizeof(restOfLine[0]);
+
+	fgets(restOfLine, rolLength, stream);
+}
+
 // When searching a linked-list, we pass in the list and the key we are looking 
 // for. If a node contaning this key found, the function returns a pointer to this
 // LL node, which contains the matching (key,value) pair:
@@ -26,7 +35,7 @@ typedef STUDENT LLValue;
 
 typedef struct LLNode 
 {
-	LLKey key;
+	LLKey Key;
 	LLValue Value;
 	struct LLNode *Next;
 } LLNode;
@@ -70,7 +79,68 @@ int LLCompareKeys (LLKey key1, LLKey key2)
 		return +1;
 }
 
-int main () 
+//LLSearch: performs a linear search of the given linked-list, looking for the first
+//node that contains a matching key. Returns a pointer to this node if found, otherwise NULL
+//is returned.
+
+LLNode *LLSearch ( LL *list, LLKey key)
 {
+
+	LLNode * ptr = (LLNode *)malloc(sizeof(LLNode));
+	ptr = list -> Head;
+	while ( ptr != NULL ) 
+	{
+		if (LLCompareKeys ( key , ptr -> Key) == 0){
+			return ptr;
+		}
+		else ptr = ptr->Next;
+	}
+
+
+	//LLNode *ptr = LL->head;
+	return NULL; // not found
+}
+
+
+int main (int argc, char *argv[]) 
+{
+
+	char netid[16];
+	int mid, fnl;
+
+	LL *list = LLCreate();
+	LLNode *cur = NULL;
+	scanf("%s %d %d", netid, &mid, &fnl);
+	skipRestOfInput(stdin);
+
+
+	while ( netid[0] != '#')
+	{ STUDENT s;
+		s.Netid = (char *)malloc((strlen(netid) +1) * sizeof(char));
+		strcpy(s.Netid,netid);
+		s.Midterm = mid;
+		s.Final = fnl;
+
+		LLNode *node;
+		node =(LLNode *)malloc(sizeof(LLNode));
+		node->Key = s.Netid;
+		node->Value = s;
+		node->Next = NULL;
+
+		if (cur == NULL) // first node
+		{ list -> Head = node;
+			cur = node;
+		}
+		else 
+		{
+			cur->Next =node;
+			cur = node;
+		}
+		list -> Count ++;
+		scanf("%s %d %d", netid, &mid, &fnl);
+		skipRestOfInput(stdin); 
+	} 
+
+
 	return 0;
 }
