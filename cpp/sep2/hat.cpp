1b72b4b1dcec587b12d9b1db44e11955f3da7646
=Uladzimir Sapeshka <usapeshka@gmail.com>
2018-07-19 06:35:47 -0500

    Simple Recursion example is added

---
+++ cpp/sep2/hat.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void removeHat(char cat) {
+	for (char c = 'A'; c < cat ; c++ )
+		cout << "   ";
+	if(cat <= 'Z') {
+		cout << " cat " << cat << endl;
+		removeHat(cat+1); //Recursive call
+	} else
+		cout << "VOOM!!!" <<endl;
+}
+
+int main() {
+	removeHat('A');
+} /// :~
