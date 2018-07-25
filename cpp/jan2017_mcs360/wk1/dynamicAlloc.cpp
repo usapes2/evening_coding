4a25c7aeae2d46fef6e0c81487d9699655044740
=Uladzimir Sapeshka <usapeshka@gmail.com>
2018-07-25 12:10:07 -0500

    dynamic allocation example added

---
+++ cpp/jan2017_mcs360/wk1/dynamicAlloc.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+	int n;
+	cout << " give the dimension : " ;
+	cin >> n;
+
+	cout << " allocating an array of length " << n << endl;
+
+	double a[n];
+	for ( int i = 0 ; i < n ; i++ ) a[i] = double (i);
+	
+	// Dynamic allocation 
+	
+	double *b ; // create a pointer
+	b = new double[n]; // dynamic allocation
+	for ( int i = 0 ; i < n ; i ++ ) b[i] = i;
+	
+	// Printing the results
+	
+	for ( int i = 0 ; i < n ; i ++ ) {
+		cout << "a[" << i << "] = " << a[i] ;
+		cout << endl; }
+
+	cout << " \n" ;
+	for ( int i = 0 ; i < n ; i ++ ) {
+		cout << "b[" << i << "] = " << b[i] ;
+		cout << endl; }
+	return 0;
+}
