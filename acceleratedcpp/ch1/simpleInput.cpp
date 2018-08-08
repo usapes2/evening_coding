c62126f9f843c015171e67f6e92eaefd1df26a09
=Uladzimir Sapeshka <usapeshka@gmail.com>
2018-08-08 14:41:17 -0500

    Simple input example

---
+++ acceleratedcpp/ch1/simpleInput.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main (){
+	string s;
+	string ss;
+
+	cout<<"Please enter a string " << endl;
+	cin>>s;
+	cout<<"Please enter a string " << endl;
+	cin>>ss;
+	cout << "s "<<s<<endl;
+	cout << "ss "<<ss<<endl;
+	
+
+	return 0;
+}
