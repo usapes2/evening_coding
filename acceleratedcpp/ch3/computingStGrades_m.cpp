d06e825d10b1d72ee19f20bc49681729764dd9af
=Uladzimir Sapeshka <usapeshka@gmail.com>
2018-08-16 00:43:01 -0500

    final exam grades using vector bugs fixing

--- acceleratedcpp/ch3/computingStGrades_m.cpp
+++ acceleratedcpp/ch3/computingStGrades_m.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using std::cin;
 using std::cout;
@@ -11,6 +12,8 @@ using std::setprecision;
 using std::string;
 using std::streamsize;
 using std::vector;
+using std::sort;
+
 
 //using namespace std;
 int main () {
@@ -37,20 +40,29 @@ int main () {
 	double x;
 	vector<double> hw;
 
-
-	// invariant:
-	// we have read count grades so far, and
-	// sum is the sum of the first count grades
-	/* Next condition implicitly uses an istream as the subject of the while condition: 
-	 if ( cin >> x) { * .... }  is the same statement as cin >>x ; if ( cin ) { ... }
-	 cin has a type of istream. */
-	
 	while(cin>>x) 
 		hw.push_back(x);
 
+	// check that the student entered some hw grades
+	typedef vector<double>::size_type vec_sz;
+	vec_sz size = hw.size();
+
+	if ( size == 0 ) {
+		cout << endl << " You must enter your grades. " 
+			"Please try again." << endl;
+		return 1;
+	}
+
+
+	// sort the grades
+	sort(hw.begin(),hw.end());
+	// comput the median
+        vec_sz mid = size / 2;
+	double median;
+	median = size % 2 == 0 ? (hw[mid] * hw[mid-1] ) /2 : hw[mid];
 	// write the result 
 	streamsize prec = cout.precision(); // presision is a member function of cout is used to reset the precision.
-	cout << "Your final grade is "<< setprecision(3)<<0.2*midterm+0.4*final+0.4*sum/count<<setprecision(prec)<<endl;
+	cout << "Your final grade is "<< setprecision(3)<<0.2*midterm+0.4*final+0.4*median<<setprecision(prec)<<endl;
 	// setprecision is a manipulator.It manipulates the stream by causing subsequent output on that stream to appear with the given 
 	// numver of sig digits.
 
