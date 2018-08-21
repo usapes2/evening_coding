66dfe7eb99b280dc9aa7263ea780510ff0af8dc6
=Uladzimir Sapeshka <usapeshka@gmail.com>
2018-08-21 18:35:34 -0500

    Organizigng the data for the stGrade problem

---
+++ acceleratedcpp/ch3/newfunc.cpp
@@ -0,0 +1,33 @@
+
+
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::setprecision;
+using std::string;
+using std::streamsize;
+using std::vector;
+using std::sort;
+using std::domain_error;
+using std::istream;
+
+
+//using namespace std;
+//
+struct Student_info { 
+	string name;
+	double midterm, final;
+	vector<double> homework;
+}; // note the semicolon 
+int main () {
+
+	return 0;
+}
